2a6b0d4878bda1a835b72cbd5f9708ee1afeef6b
Umawadee <ta-n2214@hotmail.com>
2018-02-23 12:02:04 +0700

    loginnnnn

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,10 @@
 package sample;
 
+import admin.adminController;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -11,13 +13,13 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
-    private loginModel loginModel = new loginModel();
+public class Controller implements Initializable{
+    private loginModel loginmodel = new loginModel();
 
     @FXML
     private JFXTextField username;
@@ -26,7 +28,7 @@ public class Controller implements Initializable {
     private JFXPasswordField password;
 
     @FXML
-    private JFXButton btilogin;
+    private JFXButton btlogin;
 
     @FXML
     private Label loginStatus;
@@ -37,58 +39,46 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        if (this.loginModel.isDatabaseConnection()){
-            this.dbStatus.setText("Connected to Database");
-        }else {
-            this.dbStatus.setText("Not Connected to Database");
-        }
 
-        this.combobox.setItems(FXCollections.observableArrayList(option.values()));
-    }
+        if (this.loginmodel.isDatabaseConnection()){
+            this.dbStatus.setText("Connected to DB.");
+
+        }else{
+            this.dbStatus.setText("Not Connect to DB.");
+        }
+    }//initialii
     @FXML
-    public void Login(ActionEvent event){
-
-        System.out.println(this.username.getText());
-        System.out.println(this.password.getText());
-        System.out.println(this.combobox.getValue().toString());
-        try {
-            if
-                    (this.loginModel.isLogin(this.username.getText(),this.password.getText(),
-                    this.combobox.getValue().toString())){
-                Stage stage = (Stage) this.btnLogin.getScene().getWindow();
+    public  void  Login(ActionEvent event){
+        try{
+            if (this.loginmodel.isLogin(username.getText(),password.getText())){
+                Stage stage = (Stage) this.btnlogin.getScene().getWindow();
                 stage.close();
-                switch (this.combobox.getValue().toString()){
-                    case "Admin": adminLogin(); break;
-                    case "Student": studentLogin(); break;
-                }
-            }else{
-                this.loginStatus.setText("Wrong Credential");
+                adminDashborad();
+
+            }else {
+                loginStatus.setText("Your username or password is inva");
             }
-        } catch (Exception localException) {
-            localException.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    }
-    public void adminController(){
-        try {
-            Stage userStage = new Stage();
-            FXMLLoader loader = new FXMLLoader();
-            Pane root = (Pane)
-                    loader.load(getClass().getResource("/admin/adminDashboard.fxml").openStream
-                            ());
-             adminController adminController.
-                    loader.getController();
-            Scene scene = new Scene(root);
-            userStage.setScene(scene);
-            userStage.setTitle("Student Dashboard");
-            userStage.setResizable(false);
-            userStage.show();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-    public void adminLogin(){
-        try {
+
+    }//isLonig
+
+    private void adminDashborad() {
+        try{
             Stage adminStage = new Stage();
             FXMLLoader adminLoader = new FXMLLoader();
+            Pane adminRoot = (Pane) adminLoader.load(getClass().getResource("/admin/adminDashboard.fxml").openStream());
+            adminController admincontroller = adminLoader.getController();
+            Scene scene = new Scene(adminRoot);
+            adminStage.setScene(scene);
+            adminStage.setTitle("Admin Dashborad");
+            adminStage.setResizable(false);
+            adminStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    }
+
+
+    }//adminDashborad
+}//class
--- src/sample/loginModel.java
+++ src/sample/loginModel.java
@@ -2,7 +2,6 @@ package sample;
 
 import dbUtil.dbConnection;
 
-import javax.xml.transform.Result;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -17,37 +16,37 @@ public class loginModel {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        if (this.connection == null) {
+        if (this.connection == null){
             System.exit(1);
         }
+
+    }//loginModel
+    public  boolean isDatabaseConnection(){
+        return  this.connection !=null;
     }
-    public boolean isDatabaseConnection(){
-        return this.connection != null;
-    }
-    public boolean isLogin(String user, String pass, String opt) throws
-            Exception{
+    public  boolean isLogin (String user, String pass) throws SQLException {
         PreparedStatement pr = null;
         ResultSet rs = null;
-        String sql = "SELECT * FROM login where username = ? and password =
-                ? and division = ?";
+
+        String sql = "select * from user where username =? and password = ?";
         try {
             pr = this.connection.prepareStatement(sql);
             pr.setString(1,user);
             pr.setString(2,pass);
-            pr.setString(3,opt);
             rs = pr.executeQuery();
-            boolean boll1;
             if (rs.next()){
-                return true;
+
+                return  true;
             }
             return false;
-        }catch (SQLException ex){
-            ex.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
             return false;
-        }
-        finally {
+        }finally {
             pr.close();
             rs.close();
         }
-    }
-}
+
+
+    }//login
+}//class
